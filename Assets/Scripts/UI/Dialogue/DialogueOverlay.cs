caed4668e1a40ef67561226ab75af18cf8989e3a
Ryan Andersen <randersen3@gatech.edu>
2017-04-30 13:54:01 -0400

    Dialogue Overlay added into scene

---
+++ Assets/Scripts/UI/Dialogue/DialogueOverlay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueOverlay : MonoBehaviour {
+    public Text nameBoxLeft;
+    public Text nameBoxRight;
+    public Text mainTextBox;
+    public Image portraitLeft;
+    public Image portraitRight;
+
+}
